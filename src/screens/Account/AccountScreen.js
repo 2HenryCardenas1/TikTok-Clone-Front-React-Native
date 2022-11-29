289cc0d258c7c564919c3fb447966d8f13796bec
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-11-29 18:20:13 -0500

    Screens created

---
+++ src/screens/Account/AccountScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-elements'
+
+export function AccountScreen(props) {
+    return (
+        <View>
+            <Text>AccountScreen</Text>
+
+        </View>
+    )
+}
---
+++ src/screens/Account/index.js
@@ -0,0 +1,7 @@
+export * from './AccountScreen';
+export * from './ChangeDescriptionScreen';
+export * from './ChangeInstagram';
+export * from './ChangeNameScreen';
+export * from './ChangeWebSiteScreen';
+export * from './SettingsScreen';
+
