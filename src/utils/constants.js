48c553f47f08241e9132b050b841da8e152cb076
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-11-29 18:19:32 -0500

    Screens names changed

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,15 +2,15 @@ const SERVER_IP = "IP:8000"
 
 export const ENV = {
     BASE_PATH: `http://${SERVER_IP}`,
-    BASE_API : `http://${SERVER_IP}/api`,
-    API_ROUTES : {
-        REGISTER : "auth/register",
-        LOGIN : "auth/login",
-        REFRESH_TOKEN : "auth/refresh_token",
-    }, 
-    JWT : {
-        ACCESS_TOKEN : "access",
-        REFRESH_TOKEN : "refresh"
+    BASE_API: `http://${SERVER_IP}/api`,
+    API_ROUTES: {
+        REGISTER: "auth/register",
+        LOGIN: "auth/login",
+        REFRESH_TOKEN: "auth/refresh_token",
+    },
+    JWT: {
+        ACCESS_TOKEN: "access",
+        REFRESH_TOKEN: "refresh"
     }
 }
 
--- src/utils/screens.js
+++ src/utils/screens.js
@@ -1,9 +1,58 @@
 const authScreens = {
-    auth : "auth",
-    registerEmail : "Registro de usuario",
-    loginEmail : "LoginEmail",
+    auth: "auth",
+    registerEmail: "RegisterEmail",
+    loginEmail: "LoginEmail",
+}
+
+//Screens global
+const app = {
+    tab: "Tab",
+    followers: "Followers",
+    followed: "Followed",
+    user: "User",
+    video: "Video",
+    videosPublished: "VideosPublished",
+    videosFavorites: "VideosFavorites",
+
+}
+
+//Screens to stack
+const home = {
+    tab: "HomeTab",
+    home: "Home",
+}
+const friends = {
+    tab: "FriendsTab",
+    friends: "Friends",
+}
+
+const uploads = {
+    tab: "UploadsTab",
+    uploads: "Uploads",
+    publishVideo: "PublishVideo",
+}
+
+const notification = {
+    tab: "NotificationTab",
+    notification: "Notification",
+}
+
+const account = {
+    tab: "AccountTab",
+    account: "Account",
+    settings: "Settings",
+    changeName: "ChangeName",
+    changeDescription: "ChangeDescription",
+    changeWebSite: "ChangeWebSite",
+    changeInstagram: "ChangeInstagram",
 }
 
 export const screens = {
     authScreens,
+    app,
+    home,
+    friends,
+    uploads,
+    notification,
+    account,
 }
