d8f7e1a675be41dbf62cd363247391a73239641b
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-04 11:53:54 -0500

    ROUTE video implemented

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const SERVER_IP = "IP:8000"
+const SERVER_IP = "IP"
 
 export const ENV = {
     BASE_PATH: `http://${SERVER_IP}`,
@@ -7,10 +7,12 @@ export const ENV = {
         REGISTER: "auth/register",
         LOGIN: "auth/login",
         REFRESH_TOKEN: "auth/refresh_token",
+        VIDEO: 'video'
     },
     JWT: {
         ACCESS_TOKEN: "access",
         REFRESH_TOKEN: "refresh"
-    }
+    },
+
 }
 
