2a9501abb782286cf7898d53c9ea8ea67609c360
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-11 12:03:49 -0500

    Info and vio more option implemented

---
+++ src/components/Shared/VideoFeed/VideoFeed/Info/Info.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-elements'
+import { styles } from './Info.styles'
+export function Info(props) {
+    const { username, description } = props
+    const [showLines, setShowLines] = useState(2)
+    const openCloseDescription = () => {
+        setShowLines(showLines === 2 ? 100 : 2)
+    }
+
+    return (
+        <View style={styles.content}>
+            <Text style={styles.username}>@{username}</Text>
+            <Text numberOfLines={showLines}>{description}</Text>
+            <Text style={styles.loadMore} onPress={openCloseDescription}>{
+                showLines === 2 ? 'View more' : 'View less'
+            }
+            </Text>
+        </View>
+    )
+}
