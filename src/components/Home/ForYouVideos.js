bb268d58e1041b59407f51273ec93bbf120c814e
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-11 11:09:48 -0500

    Index to video checked

--- src/components/Home/ForYouVideos.js
+++ src/components/Home/ForYouVideos.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Dimensions, FlatList } from 'react-native'
 import { Video } from '../../api'
 import { VideoFeed } from '../../components/Shared'
@@ -12,7 +12,9 @@ const { height } = Dimensions.get('window')
 export function ForYouVideos() {
 
     const [videos, setVideos] = useState(null)
+    const [indexStart, setIndexStart] = useState(null)
     const { accessToken } = useAuth()
+
 
     useEffect(() => {
         if (accessToken) {
@@ -27,7 +29,13 @@ export function ForYouVideos() {
         }
     }, [accessToken])
 
+    const onViewChangeRef = useRef(({ viewableItems }) => {
+        setIndexStart(viewableItems[0].index)
+    })
+
+
     if (!videos) return null
+
 
     return (
         <FlatList
@@ -35,13 +43,16 @@ export function ForYouVideos() {
             decelerationRate="fast"
             keyExtractor={(_, index) => index.toString()}
             renderItem={({ item, index }) => (
-                <VideoFeed index={index} item={item} />)}
+                <VideoFeed index={index} item={item} indexShow={indexStart} />)}
             removeClippedSubviews={false}
             showsVerticalScrollIndicator={false}
             snapToInterval={height - ENV.TAB_MENU_HEIGHT}
+            onViewableItemsChanged={onViewChangeRef.current}
+            initialScrollIndex={indexStart}
             viewabilityConfig={{
                 viewAreaCoveragePercentThreshold: 50,
             }}
+
 
             onScrollToIndexFailed={() => { }}
         />
--- src/components/Shared/VideoFeed/VideoFeed/VideoFeed.js
+++ src/components/Shared/VideoFeed/VideoFeed/VideoFeed.js
@@ -1,16 +1,27 @@
+import { useFocusEffect } from '@react-navigation/native'
 import { Video } from 'expo-av'
-import React, { useRef, useState } from 'react'
+import React, { useCallback, useRef, useState } from 'react'
 import { Pressable, View } from 'react-native'
 import { Text } from 'react-native-elements'
 import { styles } from './VideoFeed.styles'
 
 export function VideoFeed(props) {
-    const { item, index } = props
+    const { item, index, indexShow } = props
     const video = useRef(null)
     const [isStarted, setIsStarted] = useState(false)
     const startPauseVideo = () => {
         setIsStarted(!isStarted)
     }
+    useFocusEffect(
+        useCallback(() => {
+            setIsStarted(index === indexShow)
+
+            return () => {
+                setIsStarted(false)
+            }
+        }, [index, indexShow])
+    )
+
     return (
         <Pressable style={styles.container} onPress={startPauseVideo}>
             <Video
