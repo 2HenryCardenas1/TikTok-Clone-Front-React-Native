149f2625bb509ec3412edd3f936922735ca8bcff
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-21 15:33:21 -0500

    Screen created

---
+++ src/screens/Account/AccountScreen/AccountScreen.js
@@ -0,0 +1,71 @@
+import { useFocusEffect } from '@react-navigation/native'
+import { capitalize } from 'lodash'
+import React, { useCallback, useState } from 'react'
+import { ScrollView } from 'react-native'
+import { Tab, TabView, Text } from 'react-native-elements'
+import { User } from '../../../api'
+import { Account } from '../../../components/Account'
+import { useAuth } from '../../../hooks'
+import { styles } from './AccountScreen.styles'
+
+const userApi = new User()
+
+export function AccountScreen(props) {
+    const { navigation } = props
+
+    const [tabActive, setTabActive] = useState(0)
+    const [user, setUser] = useState(null)
+    const { accessToken } = useAuth()
+
+
+
+    useFocusEffect(
+        useCallback(() => {
+            (async () => {
+                try {
+                    const response = await userApi.me(accessToken)
+                    navigation.setOptions({ title: capitalize(response.first_name) })
+                    setUser(response)
+                } catch (error) {
+                    console.error(error)
+                }
+            })()
+        }, [])
+    )
+
+    if (!user) return null
+
+    return (
+        <ScrollView>
+            <Account.Header avatar={user.avatar} username={user.username} />
+            <Account.Follows idUser={user.id} />
+
+
+            <Tab value={tabActive} onChange={(e) => setTabActive(e)} indicatorStyle={styles.indicatorStyle}>
+
+                <Tab.Item label="Follow" containerStyle={styles.containerTabStyle}
+                    icon={{
+                        type: 'material-community',
+                        name: 'grid',
+                    }}
+                />
+                <Tab.Item label="Follow" containerStyle={styles.containerTabStyle}
+                    icon={{
+                        type: 'material-community',
+                        name: 'format-list-bulleted',
+                    }}
+                />
+            </Tab>
+
+            <TabView value={tabActive} onChange={(e) => setTabActive(e)}>
+                <TabView.Item>
+                    <Text>Follow</Text>
+                </TabView.Item>
+                <TabView.Item>
+                    <Text>Favorites</Text>
+                </TabView.Item>
+            </TabView>
+
+        </ScrollView>
+    )
+}
---
+++ src/screens/Account/AccountScreen/AccountScreen.styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    indicatorStyle: {
+        backgroundColor: '#fff'
+    },
+    containerTabStyle: {
+        backgroundColor: 'transparent'
+    }
+});
