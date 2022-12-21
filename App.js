52abdc858a38157120f628bae79d209f1e88f5e5
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-21 01:09:07 -0500

    intl added

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
+import 'intl';
+import 'intl/locale-data/jsonp/es';
 import { AuthProvider, ThemeProvider } from './src/context';
-import { RootNavigation } from './src/navigation/RootNavigation'
-import './src/utils/yup-methods'
+import { RootNavigation } from './src/navigation/RootNavigation';
+import './src/utils/yup-methods';
 
 export default function App() {
   return (
