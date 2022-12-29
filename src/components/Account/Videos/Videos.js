3f771cfc6963494d75db991f78cbe11035c66505
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2022-12-29 13:11:51 -0500

    Videos component created

---
+++ src/components/Account/Videos/Videos.js
@@ -0,0 +1,55 @@
+import { useNavigation } from '@react-navigation/native'
+import { map, size } from 'lodash'
+import React, { useEffect, useState } from 'react'
+import { Pressable, View } from 'react-native'
+import { Image, Text } from 'react-native-elements'
+import { Video } from '../../../api'
+import { useAuth } from '../../../hooks'
+import { screens } from '../../../utils'
+import { styles } from './Videos.styles'
+
+const videoApi = new Video()
+export function Videos(props) {
+    const { idUser } = props
+    const { auth, accessToken } = useAuth()
+
+    const [videos, setVideos] = useState(null)
+    const navigation = useNavigation()
+
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await videoApi.getVideoUser(accessToken, idUser)
+                    setVideos(response)
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+        )()
+    }, [])
+
+    if (!videos) return null
+
+    const goToVideo = (video) => {
+        navigation.navigate(screens.app.videosPublished, { idUser, idVideo: video.id })
+    }
+    return (
+        <View style={styles.content}>
+
+            {map(videos, (video, index) => (
+                <Pressable key={index} style={styles.videoBlock} onPress={() => goToVideo(video)}>
+                    <Image source={{ uri: video.image }} style={styles.image} />
+                </Pressable>
+            ))
+            }
+
+            {size(videos) === 0 && (
+                <View style={styles.noVideos}>
+                    <Text>You have no videos published</Text>
+                </View>
+            )
+            }
+        </View>
+    )
+}
--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -2,9 +2,11 @@ import { Follows } from './Follows'
 import { Header } from './Header'
 import { Info } from './Info'
 import { Settings } from './Settings'
+import { Videos } from './Videos'
 export const Account = {
     Header,
     Follows,
     Settings,
-    Info
+    Info,
+    Videos
 }
