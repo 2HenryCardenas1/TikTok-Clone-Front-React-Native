61782c85e06a4cc63a8527704164c09ceae3eebe
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-01-05 17:17:14 -0500

    UserVideo published implemented

--- src/screens/Shared/VideosPublishedScreen.js
+++ src/screens/Shared/VideosPublishedScreen.js
@@ -1,11 +1,62 @@
-import React from 'react'
-import { View } from 'react-native'
-import { Text } from 'react-native-elements'
+import { forEach } from 'lodash'
+import React, { useEffect, useRef, useState } from 'react'
+import { Dimensions, FlatList } from 'react-native'
+import { Video } from '../../api'
+import { VideoFeed } from '../../components/Shared'
+import { useAuth } from '../../hooks'
 
-export function VideosPublishedScreen() {
+const videoApi = new Video()
+const { height } = Dimensions.get('window')
+export function VideosPublishedScreen(props) {
+
+    const { route: { params } } = props
+    const { accessToken } = useAuth()
+    const [videos, setVideos] = useState(null)
+    const [indexStart, setIndexStart] = useState(null)
+    useEffect(() => {
+        (
+            async () => {
+                try {
+                    const response = await videoApi.getVideoUser(accessToken, params.idUser)
+                    setVideos(response)
+                } catch (error) {
+                    console.error(error)
+                }
+            }
+        )()
+    }, [params])
+
+
+    useEffect(() => {
+        if (videos) {
+            forEach(videos, (video, index) => {
+                if (video.id === params.idVideo) {
+                    setIndexStart(index)
+                }
+            })
+        }
+    }, [params, videos])
+
+    const onViewChangeRef = useRef(({ viewableItems }) => {
+
+        setIndexStart(viewableItems[0].index)
+    })
+
+    if (!videos || indexStart === null) return null
     return (
-        <View>
-            <Text>VideosPublishedScreen</Text>
-        </View>
+        <FlatList
+            data={videos}
+            decelerationRate="fast"
+            keyExtractor={(_, index) => index.toString()}
+            renderItem={({ item, index }) => (<VideoFeed style={{ height: height }} item={item} index={index} indexShow={indexStart} />)}
+            removeClippedSubviews={false}
+            showsVerticalScrollIndicator={false}
+            snapToInterval={height}
+            onViewableItemsChanged={onViewChangeRef.current}
+            viewabilityConfig={{ viewAreaCoveragePercentThreshold: 50 }}
+            initialScrollIndex={indexStart}
+            onScrollToIndexFailed={() => { }}
+
+        />
     )
 }
