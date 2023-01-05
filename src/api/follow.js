2719d4a7f6668bc9080d3a87d6a75c1c972194de
2HenryCardenas1 <cardenashenryesteban@gmail.com>
2023-01-05 14:04:35 -0500

    endpoints follow implemented

--- src/api/follow.js
+++ src/api/follow.js
@@ -1,3 +1,4 @@
+import { size } from 'lodash'
 import { ENV } from '../utils'
 
 export class Follow {
@@ -33,6 +34,89 @@ export class Follow {
         if (response.status !== 200) throw result
 
         return result.followers_count
+
+    }
+
+    async followUser(token, userId, idUserFollowed) {
+        const filter = `user=${userId}&user_followed=${idUserFollowed}`
+        const url = `${ENV.BASE_API}/${ENV.API_ROUTES.FOLLOW}/?${filter}`
+
+        const params = {
+            headers: {
+                Authorization: `Bearer ${token}`
+
+            }
+        }
+
+        const response = await fetch(url, params)
+        const result = await response.json()
+
+        if (response.status !== 200) throw result
+
+        if (size(result) === 0) return false
+
+        return true
+
+    }
+
+    async follow(token, userId, idUserFollowed) {
+        const url = `${ENV.BASE_API}/${ENV.API_ROUTES.FOLLOW}/`
+
+        const params = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                user: userId,
+                user_followed: idUserFollowed
+            })
+        }
+
+        const response = await fetch(url, params)
+        const result = await response.json()
+
+        if (response.status !== 201) throw result
+
+        return result
+
+
+
+    }
+    async unFollow(token, idFollow) {
+        const utl = `${ENV.BASE_API}/${ENV.API_ROUTES.FOLLOW}/${idFollow}/`
+        const params = {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        const response = await fetch(utl, params)
+
+
+        if (response.status !== 204) throw "Error..."
+
+        return true
+
+
+    }
+
+    async getFollowing(token, userId, idUserFollower) {
+        const filter = `user=${userId}&user_follower=${idUserFollower}`
+        const url = `${ENV.BASE_API}/${ENV.API_ROUTES.FOLLOW}/?${filter}`
+        const params = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+        const response = await fetch(url, params)
+        const result = await response.json()
+
+        if (response.status !== 200) throw result
+
+        return result[0]
 
     }
 }
